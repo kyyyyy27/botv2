9abc4f0956b846f2fcec79d575cdceee461e7ca4
kyyyyy27 <107806866+kyyyyy27@users.noreply.github.com>
2022-08-21 22:01:01 +0700

    Update bot off.js

--- plugins/bot off.js
+++ plugins/bot off.js
@@ -1,7 +1,7 @@
 let handler = async (m, { participants }) => {
     // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.db.data.chats[m.chat].isBanned = true
-    m.reply(' *Yaudah kalo gitu, Kanna mau tidur dulu kak🥱😴* ')
+    m.reply(' *Yaudah kalo gitu, Zeroo mau tidur dulu kak🥱😴* ')
     // } else m.reply('Ada nomor Ownerku disini...')
 }
 handler.help = ['Kanna(on/off)']
